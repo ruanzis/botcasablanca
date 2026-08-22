25e21ac384ebc233e2e81afbc7bcf8cce9a542a1
ruanzis <125867225+ruanzis@users.noreply.github.com>
2026-08-22 16:25:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ from telegram.ext import (
     filters,
 )
 
+python-telegram-bot>=20.0
+fastapi
+uvicorn
+requests
+
+web: python main.py
+
 # Configuração de Logs
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
